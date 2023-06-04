1c01cbfb754885abce049cc4f4353e4f4d0cb59b
MertRam35 <mert.ramadan35@gmail.com>
2023-06-04 15:29:50 +0300

    initial commit

---
+++ src/reviews/week_15/map/MapIntro.java
@@ -0,0 +1,14 @@
+package reviews.week_15.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapIntro {
+
+    public static void main(String[] args) {
+
+
+        Map<Integer,String> employeeWithNum =  new HashMap<>();
+
+    }
+}
